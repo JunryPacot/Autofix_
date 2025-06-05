4ba6ef957cb8ed12b8ffdb8cfbe54662ea2f5077
JunryPacot <junrypacot878@gmail.com>
2025-06-04 17:47:56 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-'use strict';
+ const navbar = document.querySelector("[data-navbar]");
+    const navToggler = document.querySelector("[data-nav-toggler]");
 
+    navToggler.addEventListener("click", function () {
+      navbar.classList.toggle("active");
+      this.classList.toggle("active");
+    });
 
-
-/**
- * MOBILE NAVBAR TOGGLE
- */
-
-const navbar = document.querySelector("[data-navbar]");
-const navToggler = document.querySelector("[data-nav-toggler]");
-
-navToggler.addEventListener("click", function () {
-  navbar.classList.toggle("active");
-  this.classList.toggle("active");
-});
-
- if (localStorage.getItem('isLoggedIn') !== 'true') {
-    window.location.href = 'Loginpage.html';
-  }
-
-  // On logout
-  localStorage.removeItem('isLoggedIn');
-  window.location.href = 'Loginpage.html';
+    // Redirect to login if not logged in
+    if (localStorage.getItem('isLoggedIn') !== 'true') {
+      window.location.href = 'Loginpage.html';
+    }
